b92bc968e396633702c7df8f9396a66f07eda644
luislndch <luislndch@gmail.com>
2026-05-02 14:56:57 +0800

    added treesitter config file

---
+++ plugin/treesitter.lua
@@ -0,0 +1,4 @@
+require('nvim-treesitter').setup {
+  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+  install_dir = vim.fn.stdpath('data') .. '/site'
+}
