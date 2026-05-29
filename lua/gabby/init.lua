c2ba37b6332d1c7f008ed0e4b620320587d670c7
Luis Landicho <luislndch@gmail.com>
2026-05-29 22:45:07 +0800

    added nerdtree ocnfig file

--- lua/gabby/init.lua
+++ lua/gabby/init.lua
@@ -5,12 +5,6 @@ vim.api.nvim_create_autocmd('StdinReadPre',{
     command = "let s:std_in=1"
 })
 
-vim.api.nvim_create_autocmd('VimEnter',{
-    pattern = {"*"},
-    callback = function()
-	vim.cmd[[if argc() == 0 && !exists('s:std_in') && v:this_session == '' | NERDTree | endif]]
-    end
-})
 
 require('gabby.remaps')
 
