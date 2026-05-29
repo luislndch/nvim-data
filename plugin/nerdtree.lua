48acbd8224a164522d1a3b92e4ec69267aa334a6
Luis Landicho <luislndch@gmail.com>
2026-05-29 22:52:13 +0800

    fixed errors in nerdtree.lua

---
+++ plugin/nerdtree.lua
@@ -0,0 +1,14 @@
+vim.keymap.set('n','<leader>n',':NERDTreeFocus<CR>',{silent=true});
+vim.keymap.set('n','<C-n>',':NERDTreeToggle<CR>',{silent=true});
+vim.keymap.set('n','<C-f>',':NERDTreeFind<CR>',{silent=true});
+
+--open NERDTree at startup
+vim.api.nvim_create_autocmd('VimEnter',{
+	pattern = {"*"},
+	callback = function()
+		if vim.fn.argc() == 0 then
+		  vim.cmd("NERDTree")
+		end
+	end
+})
+
